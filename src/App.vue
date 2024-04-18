b3816f01ac92affacc556c90cc43d94134b1dff2
Vineet Lazarus <vinxldev@gmail.com>
2024-04-19 00:27:18 +0530

    setup code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ejs-grid :dataSource='data'>
+    <e-columns>
+      <e-column field='OrderID' width='100' textAlign="Right"></e-column>
+      <e-column field='CustomerID' width='100'></e-column>
+      <e-column field='EmployeeID' width='100' textAlign="Right"></e-column>
+      <e-column field='Freight' width='100' format="C2" textAlign="Right"></e-column>
+      <e-column field='ShipCountry' width='100'></e-column>
+    </e-columns>
+  </ejs-grid>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { GridComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-vue-grids'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    'ejs-grid': GridComponent,
+    'e-columns': ColumnsDirective,
+    'e-column': ColumnDirective
+  },
+  data() {
+  return {
+    data:[
+      {
+        OrderID: 10248, CustomerID: 'VINET', EmployeeID: 5, ShipCountry: 'France', Freight: 32.38 
+      },
+      {
+        OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6, ShipCountry: 'Germany', Freight: 11.61 
+      },
+      {
+        OrderID: 10250, CustomerID: 'HANAR', EmployeeID: 4, ShipCountry: 'Brazil', Freight: 65.83 
+      }
+    ],
+  };
+}
 }
 </script>
 
+
 <style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-calendars/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-grids/styles/material.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerLicense } from '@syncfusion/ej2-base'
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXtfeHVUQmlYWEN1WEc=')
 createApp(App).mount('#app')
